d8f984117545027da719719fa622dbd4e55d0f31
Сергей <040620@mail.ru>
2022-01-03 12:18:01 +0300

    Initial commit

---
+++ src/main/java/pro/sky/course2/homework2_10/skyproemployeelibraries/SkyProEmployeeLibrariesApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.course2.homework2_10.skyproemployeelibraries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyProEmployeeLibrariesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyProEmployeeLibrariesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/course2/homework2_10/skyproemployeelibraries/SkyProEmployeeLibrariesApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.course2.homework2_10.skyproemployeelibraries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyProEmployeeLibrariesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
